86fd0850d4ea655061b9bf2a6b5b3716a9258805
ericyueh <135192940+ericyueh@users.noreply.github.com>
2023-07-06 08:27:05 +0800

    Update vender.sol

--- vender.sol
+++ vender.sol
@@ -1,43 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./mytoken.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+import "./yourtoken.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
 
 // Learn more about the ERC20 implementation 
 // on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vendor is Ownable {
 
   // Our Token Contract
-  MyToken myToken;
+  Yourtoken yourToken;
 
   // token price for ETH
-  uint256 public tokensPerEth = 100;
+  uint256 public tokensPerEth = 100;  
+
+  // set TokenTransfter indicator
+  bool public isTknTransfer;
 
   // Event that log buy operation
   event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   constructor(address tokenAddress) {      
-      myToken = MyToken(tokenAddress);  
+      yourToken = Yourtoken(tokenAddress);  
   }
-
+    
   /**
   * @notice Allow users to buy token for ETH
   */
-  function buyTokens() public payable returns (uint256 tokenAmount) {
-    require(msg.value > 0, "Send ETH to buy some tokens");
+  function buyTokens() public payable returns (uint256 tokenAmount) {   
+    require(msg.value > 0, "Send ETH to buy some tokens");  
 
     uint256 amountToBuy = msg.value * tokensPerEth;
 
     // check if the Vendor Contract has enough amount of tokens for the transaction
-    uint256 vendorBalance = myToken.balanceOf(address(this));
-    require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+      uint256 vendorBalance = yourToken.balanceOf(address(this));
+      require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
 
     // Transfer token to the msg.sender
-    (bool sent) = myToken.transfer(msg.sender, amountToBuy);
+    (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
     require(sent, "Failed to transfer token to user");
+
+    // set TokenTransfter indicator
+    isTknTransfer = true;
 
     // emit the event
     emit BuyTokens(msg.sender, msg.value, amountToBuy);
@@ -55,10 +61,18 @@ contract Vendor is Ownable {
     (bool sent,) = msg.sender.call{value: address(this).balance}("");
     require(sent, "Failed to send user balance back to the owner");
   }
+
   /**
   * @notice check balance of eth in this contract
   */
   function getBalance() external view returns (uint256) {
       return address(this).balance;
+  }
+
+  /**
+  * @notice check balance of eth in this contract
+  */
+  function getTokenBalance() external view returns (uint256) {
+      return yourToken.balanceOf(address(this));
   }
 }
